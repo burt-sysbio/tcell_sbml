d8eb6e5367031d6d611dbe75bf920ce382bdcd45
burt-sysbio <philippburt@gmail.com>
2020-07-28 13:04:55 +0200

    refactor plotting module

--- code/model_analysis/plotting_module.py
+++ code/model_analysis/plotting_module.py
@@ -21,74 +21,65 @@ def plot_param_uncertainty(df, pname, save = False):
     plots two lineplots on top of each other for armstrong and cl13 
     parameter variation
     """
-    
+    def split_df(df):      
     # split up dataframe for plotting
-    arm = df[df.Infection == "Arm"]
-    cl13 = df[df.Infection == "Cl13"]
+        arm = df[df.Infection == "Arm"]
+        cl13 = df[df.Infection == "Cl13"]
+        return (arm, cl13)
     
-    arm_tfh = utils.filter_cells(arm, ["Tfh_all"])
-    cl13_tfh = utils.filter_cells(cl13, ["Tfh_all"])
-    arm_notfh = utils.filter_cells(arm, ["nonTfh"])
-    cl13_notfh = utils.filter_cells(cl13, ["nonTfh"])
+    # these are the cell types that will be plotted
+    # Tfh all will be plotted as relative fraction
+    cells = ["Tfh_all", "Th1_eff", "Tr1_all", "Tfh_eff", "Tfh_chronic"]
     
-    # create colorbar
+    # get all cells and split them up according to arm and cl13
+    df_list = [utils.filter_cells(df, [cell]) for cell in cells]
+    df_list = [split_df(df) for df in df_list]
+    
+    # create colorbar mappable
     cmap = "Blues"
-    norm = plt.Normalize(arm.val.min(), arm.val.max())
+    norm = plt.Normalize(df.val.min(), df.val.max())
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
     
     # create figure and axes
-    fig, axes = plt.subplots(3,2, figsize = (10,10))
+    fig, axes = plt.subplots(5,2, figsize = (10,14))
     
-    ax = axes.flatten()
-    
-    # tfh relative cell numbers
-    sns.lineplot(data = arm_tfh, x = "time", y = "total", hue = "val",
-                 palette = cmap, ax = ax[0])
-    
-    sns.lineplot(data = cl13_tfh, x = "time", y = "total", hue = "val",
-                 palette = cmap, ax = ax[1])
+    # create a panel with arm and cl13 infection and all celltypes as rows
+    # create lineplot with color indicating param value
+    for df, ax, cell in zip(df_list, axes, cells):
+        ylabel = cell
 
-    # tfh total cell numbers
-    sns.lineplot(data = arm_tfh, x = "time", y = "value", hue = "val",
-                 palette = cmap, ax = ax[2])
+        if cell == "Tfh_all":
+            yvar = "total"
+        else:
+            yvar = "value"
+
+        sns.lineplot(data = df[0], x = "time", y = yvar, hue = "val",
+                     palette = cmap, ax = ax[0])
     
-    
-    sns.lineplot(data = cl13_tfh, x = "time", y = "value", hue = "val",
-                 palette = cmap, ax = ax[3])
-    
-    # non tfh total cell numbers
-    sns.lineplot(data = arm_notfh, x = "time", y = "value", hue = "val",
-                 palette = cmap, ax = ax[4])
-    
-    sns.lineplot(data = cl13_notfh, x = "time", y = "value", hue = "val",
-                 palette = cmap, ax = ax[5])
-    
-   
-    reg_scale = [ax[0], ax[1]]
-    log_scale = [ax[2], ax[3], ax[4], ax[5]]
-    
+        sns.lineplot(data = df[1], x = "time", y = yvar, hue = "val",
+                     palette = cmap, ax = ax[1])
+
+        # axes specific changes
+        for a in ax:
+            if cell == "Tfh_all":
+                ylabel = "Tfh % of total"
+                a.set_ylim(0,100)
+                
+            else:
+                ylabel = cell
+                a.set_yscale("log")
+                a.set_ylim(1e0, 1e7)
+                
+            a.get_legend().remove()
+            a.set_ylabel(ylabel)
+        
+        # add colorbar to each row
+        plt.colorbar(sm, ax = [ax[0], ax[1]], label = pname)
+            
+    ax = axes.flatten()
     ax[0].set_title("Acute Infection")
     ax[1].set_title("Chronic Infection")
-    ax[2].set_ylabel("Tfh cells")
-    ax[3].set_ylabel("Tfh cells")
-    ax[4].set_ylabel("Th1 cells")
-    ax[5].set_ylabel("Th1 cells")
-            
-    for a in reg_scale:
-        a.set_ylim(0,100)
-        a.set_ylabel("Tfh % of total")
-    
-    for a in log_scale:
-        a.set_yscale("log")
-        a.set_ylim(1e0, 1e8)
-
-    for a in ax:
-        a.get_legend().remove()
-    
-    plt.colorbar(sm, ax = [ax[0], ax[1]], label = pname)
-    plt.colorbar(sm, ax = [ax[2], ax[3]], label = pname)
-    plt.colorbar(sm, ax = [ax[4], ax[5]], label = pname)
 
     if save == True:
         today = str(date.today())
